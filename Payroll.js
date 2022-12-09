6f6287e7f4b69ca6dc697d6989cc65e0e0dcb468
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-12-09 23:06:50 +0530

    Added: Saved employee payroll object to local storage

--- Payroll.js
+++ Payroll.js
@@ -63,3 +63,23 @@ const getInputElementValue = (id) => {
 let value = document.getElementById(id).value;
 return value;
 }
+// const save = () => {
+//     try{
+//         let employeePayrollData = createEmployeePayroll();
+//         createAndUpdateStorage(employeePayrollData);
+//     }
+//     catch(e){
+//         return;
+//     }
+// }
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
+}
